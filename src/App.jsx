6d46059e362ae5eed49116da548f4602d5369b20
tienpham <tpha768@gmail.com>
2026-05-17 23:31:53 +0700

    fix and release: value and investment calculator v1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 7,
+    duration: 10,
+  });
+  const inputIsValid = userInput.duration >= 1;
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevInput) => ({
+      ...prevInput,
+      [inputIdentifier]: +newValue,
+    }));
+  };
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
